4ac2ccc12c3b993c455a3402f2b354523bdcfc24
Mārtiņš Evarts Zviedris <125508859+MartinsEvartsZviedris221RDB210@users.noreply.github.com>
2023-04-02 23:19:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 class Query:
-    def _init_(self, query):
+    def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
+
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
+
 def write_responses(result):
     print('\n'.join(result))
+
 def process_queries(queries):
     result = []
+    # Keep list of all existing (i.e. not deleted yet) contacts.
     contacts = []
     for cur_query in queries:
         if cur_query.type == 'add':
+            # if we already have contact with such number,
+            # we should rewrite contact's name
             for contact in contacts:
                 if contact.number == cur_query.number:
                     contact.name = cur_query.name
                     break
-            else:
+            else: # otherwise, just add it
                 contacts.append(cur_query)
         elif cur_query.type == 'del':
             for j in range(len(contacts)):
@@ -33,5 +39,6 @@ def process_queries(queries):
                     break
             result.append(response)
     return result
-if _name_ == '__main__':
+
+if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
